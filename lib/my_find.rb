d367d08bad217afa1951d4e1f64eb47dd07b010f
Ryan Pearce <ryan.pearce@key.me>
2020-11-20 21:31:55 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,12 +1,7 @@
-require 'pry'
-
-d    def my_find(collection)
+    def my_find(collection)
       i = 0
       while i < collection.length
-        if yield(collection[i])
-          return collection[i]
-        end
+        return collection[i] if yield(collection[i])
         i = i + 1
       end
     end
-
